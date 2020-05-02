357cf71539e8ce71f105161c1d4bfd88fa0237c7
grunftz <34340186+grunftz@users.noreply.github.com>
2020-05-03 01:57:44 +0200

    Add files via upload

---
+++ STM32F10x  Date_Time_Loading/STM32F10x  Date_Time_Loading/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace STM32F10x__Date_Time_Loading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnCooPort = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // btnCooPort
+            // 
+            this.btnCooPort.Location = new System.Drawing.Point(31, 86);
+            this.btnCooPort.Name = "btnCooPort";
+            this.btnCooPort.Size = new System.Drawing.Size(163, 31);
+            this.btnCooPort.TabIndex = 0;
+            this.btnCooPort.Text = "Usart Open!";
+            this.btnCooPort.UseVisualStyleBackColor = true;
+            this.btnCooPort.Click += new System.EventHandler(this.btnCooPort_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.btnCooPort);
+            this.groupBox1.Location = new System.Drawing.Point(15, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 145);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Communication tab!";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(31, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 164);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(464, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.lblSec);
+            this.groupBox2.Controls.Add(this.lblMinutes);
+            this.groupBox2.Controls.Add(this.lblHours);
+            this.groupBox2.Controls.Add(this.btn);
+            this.groupBox2.Location = new System.Drawing.Point(239, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(217, 145);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Date - Time!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Current RTC Time!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(126, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = ":";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(73, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = ":";
+            // 
+            // lblSec
+            // 
+            this.lblSec.BackColor = System.Drawing.SystemColors.Info;
+            this.lblSec.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSec.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSec.Location = new System.Drawing.Point(141, 25);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(33, 23);
+            this.lblSec.TabIndex = 3;
+            this.lblSec.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.BackColor = System.Drawing.SystemColors.Info;
+            this.lblMinutes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMinutes.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutes.Location = new System.Drawing.Point(89, 25);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(33, 23);
+            this.lblMinutes.TabIndex = 2;
+            this.lblMinutes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHours
+            // 
+            this.lblHours.BackColor = System.Drawing.SystemColors.Info;
+            this.lblHours.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHours.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.Location = new System.Drawing.Point(36, 25);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(33, 23);
+            this.lblHours.TabIndex = 1;
+            this.lblHours.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(36, 62);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(139, 23);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "GET/SET System Time!";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(464, 186);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "STM32F10x Internal RTC Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnCooPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblSec;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ STM32F10x  Date_Time_Loading/STM32F10x  Date_Time_Loading/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Globalization;
+
+namespace STM32F10x__Date_Time_Loading
+{
+    public partial class Form1 : Form
+    {
+        //-------------------VARßs-------------------------
+        string[] comPort = SerialPort.GetPortNames();
+        string data;
+        
+        //-------------------------------------------------
+        public Form1()
+        {
+            InitializeComponent();
+            //-----------GET PORT NAMES-----------------
+            foreach (var portIme in comPort)
+            {
+                comboBox1.Items.Add(portIme);
+            }
+            comboBox1.SelectedIndex =0;
+            //-------------------------------------------
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "DISCONNECTED!";
+
+        }
+
+        private void btnCooPort_Click(object sender, EventArgs e)
+        {
+
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Open();
+                    toolStripStatusLabel1.Text = comboBox1.SelectedItem.ToString()+ " CONNECTED!";
+                    btnCooPort.Text = "Usart Close!";
+                    comboBox1.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                serialPort1.Close();
+                btnCooPort.Text = "Usart Open!";
+                toolStripStatusLabel1.Text = " DISCONNECTED!";
+                comboBox1.Enabled = true;
+
+            }
+        }
+              private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.SelectedItem.ToString();
+
+        }
+
+ 
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            
+                var time = DateTime.Now;
+                lblHours.Text = time.Hour.ToString();
+                if (time.Minute > 9)
+                {
+                    lblMinutes.Text = time.Minute.ToString();
+                }
+                else
+                {
+                    lblMinutes.Text = "0" + time.Minute.ToString();
+                }
+                
+                if (time.Second > 9)
+                {
+                    lblSec.Text = time.Second.ToString();
+                }
+                else 
+                {
+                    lblSec.Text = "0"+time.Second.ToString();
+                }
+                Console.WriteLine(DateTime.Now.ToString());
+            //conversion form time to RTC_count
+            int h = time.Hour * 3600;
+            int m = ((time.Minute * 60) + h) % 3600;
+            int midlle = h + m;
+            int s = (((time.Second) - (midlle)) % 60);
+
+            int count_RTC = h + m + (s+60);
+
+            Console.WriteLine(h);
+
+            Console.WriteLine(m);
+            Console.WriteLine(s+60);
+         
+            Console.WriteLine(count_RTC);
+            if (!serialPort1.IsOpen)
+            {
+                MessageBox.Show("Com Port Closed!");
+            }
+            else
+            {
+
+                serialPort1.Write(count_RTC.ToString().Length + count_RTC.ToString());
+            }
+               
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            data = serialPort1.ReadLine();
+            this.Invoke(new EventHandler(nekafunk));
+
+        }
+
+
+        private void nekafunk(object sender, EventArgs e)
+        {
+            textBox1.Text = data;
+        }
+
+
+
+    }
+
+    }
